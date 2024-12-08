ddb1da88129e3fe1863d774ddd16baf6ef9f383b
zia295 <74332450+zia295@users.noreply.github.com>
2024-12-08 21:51:47 +0000

    2

--- contact.php
+++ contact.php
@@ -2,15 +2,35 @@
 
 
 
-
-
-
-<form action="contact.php" method="Post" required>
-    <input class="signup-input" type="text" name="name" placeholder="your name" required><br>
+<h1 class="con-title">
+    Contact Us
+</h1>
+<br>
+<br>
+<h4 class="subtitle">Feel free to get in touch...</h4>
+<br>
+<br>
+<form class="form" action="contact.php" method="Post" required>
+    <input class="inputs" type="text" name="name" placeholder="your name" required><br>
     <br>
-        <input class="signup-input" type="text" name="email" placeholder="your email address" required><br>
+        <input class="inputs" type="text" name="email" placeholder="your email address" required><br>
         <br>
-        <input class="signup-input" type="text" name="message" placeholder="your message" required><br>
+        <input class="inputs" type="text" name="message" placeholder="your message" required><br>
         <br>
-        <button class="signup-butt" type="submit">Submit</button>
+        <button type="submit">Submit</button>
     </form>
+
+    <br>
+
+
+    <section>
+    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d9731.683824444852!2d-1.5134334!3d52.4262299!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2suk!4v1733694376773!5m2!1sen!2suk" width="300" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </section>
+
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <?php include "footer.php"; ?>
